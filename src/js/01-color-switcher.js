d45209a327e5f8a49388cbb4e2061c74aede707b
Olexandr Savchenko <a.savchenko1207@gmail.com>
2023-10-07 00:23:51 +0300

    color-switcher.js (task1)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-console.log;
+const changeColorStart = document.querySelector('[data-start]');
+const changeColorStop = document.querySelector('[data-stop]');
+const changeColorBackground = document.querySelector('body');
+changeColorStart.addEventListener('click', startSwitching);
+changeColorStop.addEventListener('click', stopSwitching);
+let timerId = null;
+
+function startSwitching() {
+  timerId = setInterval(() => {
+    changeColorBackground.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  changeColorStop.removeAttribute('disabled');
+  changeColorStart.setAttribute('disabled', true);
+}
+
+function stopSwitching() {
+  clearInterval(timerId);
+  changeColorStart.removeAttribute('disabled');
+  changeColorStop.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
